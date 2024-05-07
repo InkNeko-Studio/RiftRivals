483860df9990eddb0d55f0178d6f9a02d5d44373
DesLigaFacens <69490107+MRJackFrost@users.noreply.github.com>
2024-05-07 17:02:36 -0300

    Feat(MenuUpdate)

--- Unity/RiftRivals/Assets/Game/Shared/UI/Popup/Popup.cs
+++ Unity/RiftRivals/Assets/Game/Shared/UI/Popup/Popup.cs
@@ -6,10 +6,25 @@ namespace Game.Shared.UI.Popup
 {
     public class Popup : MonoBehaviour
     {
+        public bool check; //false se o objeto não estiver ativo no começo da cena
         public void Show()
         {
             gameObject.SetActive(true);
         }
+        public void Showandhide()
+                {
+                    if (check == true)
+                    {
+                        gameObject.SetActive(true);
+                        check = false;
+                    }
+                    else
+                    {
+                        gameObject.SetActive(false);
+                        check = true;
+                    }
+
+                }
 
         public void Hide()
         {
